7790d56f0eba295db1a0b1e77f5d13d3fd88b00f
zzzuh <zejunlin790@gmail.com>
2023-06-15 16:31:22 -0400

    wrote constructor for configuration used for solver bfs

---
+++ solver/Configuration.ts
@@ -0,0 +1,20 @@
+export class Configuration {
+    private static const GREEN_FROG: string = "G";
+    private static const RED_FROG: string = "R";
+    private static const VALID_SPOT: string = ".";
+    private static const INVALID_SPOT: string = "*";
+
+    private static row: number;
+    private static col: number;
+
+    private board: String[][];
+    private neighbors = new Set();
+
+    constructor(filename: string) {
+        try {
+
+        } catch 
+    }
+    
+
+}
---
+++ solver/Solver.ts
@@ -0,0 +1,12 @@
+import { Configuration } from "./Configuration";
+
+class Solver {
+    
+    constructor() {};
+
+    public solve(config: Configuration): Configuration[] {
+        let predecessors = new Map<Configuration, Configuration>();
+        let queue: Configuration[];
+        
+    }
+}
