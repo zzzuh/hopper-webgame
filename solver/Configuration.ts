dc3fae7527d260f1fe21214237e1e89c6b1defdc
zzzuh <zejunlin790@gmail.com>
2023-06-18 15:02:58 -0400

    implemented solution algorithm

--- solver/Configuration.ts
+++ solver/Configuration.ts
@@ -1,33 +1,32 @@
-import * as fs from "fs";
-
 export class Configuration {
-    private static const GREEN_FROG: string = "G";
-    private static const RED_FROG: string = "R";
-    private static const VALID_SPOT: string = ".";
-    private static const INVALID_SPOT: string = "*";
+    private static GREEN_FROG: string = "G";
+    private static RED_FROG: string = "R";
+    private static VALID_SPOT: string = ".";
+    private static INVALID_SPOT: string = "*";
 
     private static row: number;
     private static col: number;
 
     private board: string[][];
-    private neighbors = new Set();
+    private neighbors = new Set<Configuration>();
+    private frogCount = 0;
 
     public constructor(filename?: string, board?: string[][], startRow?: number, startCol?: number, endRow?: number, endCol?: number) {
-        if (filename) {
-            let lines = fs.readFileSync(filename);
-            let fields: string[] = (<string><unknown>lines).split("\n");
+        if (typeof filename !== 'undefined') {
+            const fs = require("fs");
+            let lines = fs.readFileSync(filename, 'utf-8').split("\n");
+            let fields = lines[0].split(" ");
 
-            Configuration.row = parseInt(fields.at(0)!);
-            Configuration.col = parseInt(fields.at(1)!);
+            Configuration.row = parseInt(fields[0]);
+            Configuration.col = parseInt(fields[1]);
 
-            let currentRow: number = 0;
-            for (let i = 2; i < fields.length; i++) {
-                let data: string[] = (<string><unknown>fields.at(i)).split(" ");
-                for (let j = 0; j < data.length; j++) {
-                    this.board[currentRow][j] = data.at(j) as string;
-                }
-                currentRow++;
+            this.board = new Array<string[]>(this.getRow());
+
+            for (let i = 0; i < this.getRow(); i++) {
+                this.board[i] = lines[i + 1].split(/\s+/).filter(Boolean);
             }
+            this.countFrogs();
+            // this.createNeighbors();
         } else {
             this.board = board!; // exclamation to ensure that board will never be null
 
@@ -38,7 +37,62 @@ export class Configuration {
             this.board[(startRow! + endRow!) / 2][(startCol! + endCol!) / 2] = Configuration.VALID_SPOT;
             this.board[startRow!][startCol!] = Configuration.VALID_SPOT;
             this.board[startRow! + rowDiff][startCol! + colDiff] = frog;
+            this.countFrogs();
         }
+    }
+
+    private countFrogs(): void {
+        for (let row = 0; row < this.getRow(); row++) {
+            for (let col = 0; col < this.getCol(); col++) {
+                if (this.board[row][col] === Configuration.GREEN_FROG) {
+                    this.frogCount++;
+                }
+            }
+        }
+    }
+
+    private createNeighbors(): void {
+        for (let row = 0; row < this.getRow(); row++) {
+            for (let col = 0; col < this.getCol(); col++) {
+                if (this.board[row][col] === Configuration.GREEN_FROG || this.board[row][col] === Configuration.RED_FROG) {
+                    if ((this.getRow() - 1) - row >= 4 && this.board[row + 4][col] === Configuration.VALID_SPOT 
+                    && this.board[(row + row + 4) / 2][(col + col) / 2] === Configuration.GREEN_FROG) { // move down 4 place
+                        this.neighbors.add(new Configuration(undefined, this.board, row, col, row + 4, col));
+                    }
+                    if ((this.getCol() - 1) - col >= 4 && this.board[row][col + 4] === Configuration.VALID_SPOT 
+                    && this.board[(row + row) / 2][(col + col + 4) / 2] === Configuration.GREEN_FROG) { // move left 4 places
+                        this.neighbors.add(new Configuration(undefined, this.board, row, col, row, col + 4));
+                    }
+                    if (col >= 4 && this.board[row][col - 4] === Configuration.VALID_SPOT 
+                    && this.board[(row + row) / 2][(col + (col - 4)) / 2] === Configuration.GREEN_FROG) { // move right 4 places
+                        this.neighbors.add(new Configuration(undefined, this.board, row, col, row, col - 4));
+                    }
+                    if (row >= 4 && this.board[row - 4][col] === Configuration.VALID_SPOT 
+                    && this.board[(row + (row - 4)) / 2][(col + col) / 2] === Configuration.GREEN_FROG) { // move up 4 places
+                        this.neighbors.add(new Configuration(undefined, this.board, row, col, row - 4, col));
+                    }
+                    if (row >= 2 && (this.getCol() - 1) - col >= 2 && this.board[row - 2][col + 2] === Configuration.VALID_SPOT
+                    && this.board[(row + row - 2) / 2][(col + col + 2) / 2] === Configuration.GREEN_FROG ) { // top right
+                        this.neighbors.add(new Configuration(undefined, this.board, row, col, row - 2, col + 2));
+                    }
+                }
+                
+            }
+
+        }
+    }
+
+    public isSolution(): boolean {
+        if (this.frogCount === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public getNeighbors(): Set<Configuration> {
+        this.createNeighbors();
+        return this.neighbors;
     }
     
     public getRow(): number {
@@ -53,5 +107,13 @@ export class Configuration {
         return this.board;
     }
 
-    
+    public printConfig(): void {
+        console.log(this.getRow(), "printed row");
+        console.log("\n");
+        console.log(this.getCol(), "printed col");
+
+        console.log("\n");
+
+        console.log(this.board);
+    }
 }
--- solver/Solver.ts
+++ solver/Solver.ts
@@ -1,12 +1,42 @@
 import { Configuration } from "./Configuration";
 
-class Solver {
-    
-    constructor() {};
+let config = new Configuration("./hoppers/hoppers-1.txt");
 
-    public solve(config: Configuration): Configuration[] {
-        let predecessors = new Map<Configuration, Configuration>();
-        let queue: Configuration[];
-        
+const solve = (configuration: Configuration): Configuration[] => {
+    let predecessors = new Map<Configuration, Configuration | null>();
+    let queue: Configuration[] = [];
+    let path: Configuration[] = [];
+    queue.push(configuration);
+    predecessors.set(configuration, null);
+
+    while(queue.length !== 0) {
+        let node: Configuration = queue.shift() as Configuration;
+        console.log(node);
+
+        if (node.isSolution()) {
+            let start: Configuration = configuration;
+            let end: Configuration = node;
+
+            if (predecessors.has(end)) {
+                let currConfig: Configuration = end;
+                while(currConfig !== start) {
+                    path.push(currConfig);
+                    currConfig = predecessors.get(currConfig) as Configuration;
+                }
+                path.push(start);
+            }
+        }
+
+        for (let neighbor of node.getNeighbors().values()) {
+            if (!predecessors.has(neighbor)) {
+                predecessors.set(neighbor, node);
+                queue.push(neighbor);
+            }
+        }
     }
+    return path;
 }
+
+let answer = solve(config);
+
+console.log(answer);
